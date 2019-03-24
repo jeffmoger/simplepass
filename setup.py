5f2e429fddad49224ceefb94d3a8da43b22c4dd3
jeffmoger <jeff-github@moger.com>
2019-03-23 19:19:24 -0700

    v

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requirements = ["pyperclip", "passlib", "cryptography"]
 
 setup(
     name="SimplePass",
-    version="1.0.1",
+    version="1.0.2",
     author="Example Author",
     author_email="jeff@moger.com",
     description="A simple password manager for the command line.",
