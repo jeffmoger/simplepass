e0208278d68a7dc84964a00f4549fdf9f6f23521
jeffmoger <jeff-github@moger.com>
2019-03-24 22:58:27 -0700

    set up folders on start

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requirements = ["pyperclip", "passlib", "cryptography"]
 
 setup(
     name="SimplePass",
-    version="1.0.7",
+    version="1.0.8",
     author="Jeff Moger",
     author_email="jeff@moger.com",
     description="A simple password manager for the command line.",
