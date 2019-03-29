0892fc6d887b817f61ee2df4d2d14276cbf54da3
jeffmoger <jeff-github@moger.com>
2019-03-28 23:00:48 -0700

    removed vault.py

--- pypass/__main__.py
+++ pypass/__main__.py
@@ -11,17 +11,10 @@ from time import sleep
 
 import pyperclip
 from passlib.hash import pbkdf2_sha256 as p_hash
-
-from vault import (
-    lock,
-    unlock,
-    getkey,
-    get_salt,
-    encrypt,
-    decrypt,
-    pickle_bytes,
-    unpickle_string
-    )
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.fernet import Fernet
 
 
 dir_ = os.path.expanduser('~') + '/SimplePass/'
@@ -29,6 +22,58 @@ dir_import = dir_ + 'import/'
 dir_export = dir_ + 'export/'
 dir_store = dir_ + 'store/'
 salt1 = b'EC6873C47AD2F3FABCCC62AF564996F3F84ECD446433DDE'
+
+
+def getkey(master):
+    password = master.encode()
+    salt = b'EC6873C47AD2F3FABCCC62AF564996F3F84ECD446433DDE'
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=100000,
+        backend=default_backend())
+    key = base64.urlsafe_b64encode(kdf.derive(password))
+    return key
+
+
+def get_salt():
+    return os.urandom(16)
+
+
+def encrypt(key, salt, password):
+    f = Fernet(key)
+    encrypted = f.encrypt(password)
+    return encrypted
+
+
+def decrypt(key, salt, password):
+    f = Fernet(key)
+    decrypted = f.decrypt(password)
+    return decrypted
+
+
+def pickle_bytes(token):
+    p = pickle.dumps(token)
+    return base64.b64encode(p).decode('ascii')
+
+
+def unpickle_string(token):
+    p = base64.b64decode(token)
+    return pickle.loads(p)
+
+
+def lock(key, salt1, salt2, password):
+    encrypted = pickle_bytes(
+        encrypt(key, salt2, encrypt(key, salt1, password)))
+    return encrypted
+
+
+def unlock(key, salt1, salt2, token):
+    decrypted = decrypt(
+        key, salt1, decrypt(key, salt2,
+                            unpickle_string(token)))
+    return decrypted.decode()
 
 
 def create_master():
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requirements = ["pyperclip", "passlib", "cryptography"]
 
 setup(
     name="SimplePass",
-    version="1.1.0",
+    version="1.1.1",
     author="Jeff Moger",
     author_email="jeff@moger.com",
     description="A simple password manager for the command line.",
