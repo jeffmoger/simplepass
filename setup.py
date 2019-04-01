33121ee7421e0bcbab318f5180a4481a10cb1898
jeffmoger <jeff-github@moger.com>
2019-03-31 23:51:18 -0700

    Added PyYAML to requirements and setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-requirements = ["pyperclip", "passlib", "cryptography"]
+requirements = ["pyperclip", "passlib", "cryptography", "PyYAML"]
 
 setup(
     name="simplepass",
